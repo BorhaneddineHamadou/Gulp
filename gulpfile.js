5cc77c25423c6276923835a2fc97b9d128796b5e
Borhaneddine <haborhaneddine@gmail.com>
2021-03-24 20:00:32 +0200

    Css Autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,16 @@
 'use strict';
 var gulp = require('gulp');
 var concat = require('gulp-concat');
+var prefix = require('gulp-autoprefixer') 
 
-gulp.task('cssConcat', function(){
+gulp.task('css', function(){
     return gulp.src('css/*.css')
            .pipe(concat('main.css'))
+           .pipe(prefix('last 2 versions'))
            .pipe(gulp.dest('dist/css'))
 })
 
-gulp.task('jsConcat', function(){
+gulp.task('js', function(){
   return gulp.src('js/*.js')
          .pipe(concat('all.js'))
          .pipe(gulp.dest('dist/js'))  
