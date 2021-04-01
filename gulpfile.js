fa039d6df6fd97e1fdb7d57e66340b446fe6e72c
Borhaneddine <haborhaneddine@gmail.com>
2021-04-01 16:20:21 +0100

    Gulp-sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,14 @@
 'use strict';
 var gulp = require('gulp');
 var concat = require('gulp-concat');
-var prefix = require('gulp-autoprefixer') 
+var prefix = require('gulp-autoprefixer');
+var sass = require('gulp-sass'); 
 
 gulp.task('css', function(){
-    return gulp.src('css/*.css')
-           .pipe(concat('main.css'))
+    return gulp.src('css/main.scss')
+           .pipe(sass({outputStyle:'compressed'}))
            .pipe(prefix('last 2 versions'))
+           .pipe(concat('test.css'))
            .pipe(gulp.dest('dist/css'))
 })
 
