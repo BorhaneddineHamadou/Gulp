f215704cd61183cadf6a721f6a465d6689f5872d
Borhaneddine <haborhaneddine@gmail.com>
2021-04-02 08:00:50 +0100

    Watch Task

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 var prefix = require('gulp-autoprefixer');
 var sass = require('gulp-sass'); 
+var connect = require('gulp-connect');
 
 gulp.task('css', function(){
     return gulp.src('css/main.scss')
@@ -10,10 +11,26 @@ gulp.task('css', function(){
            .pipe(prefix('last 2 versions'))
            .pipe(concat('test.css'))
            .pipe(gulp.dest('dist/css'))
+           .pipe(connect.reload())
 })
 
 gulp.task('js', function(){
   return gulp.src('js/*.js')
          .pipe(concat('all.js'))
-         .pipe(gulp.dest('dist/js'))  
+         .pipe(gulp.dest('dist/js'))
+         .pipe(connect.reload())  
 });
+
+gulp.task('connect', function(){
+  connect.server({
+         root:'./dist/',
+         livereload:true
+  });
+});
+
+gulp.task('watch', function(){
+  gulp.watch('css/*.scss', ['css']);
+  gulp.watch('js/*.js', ['js']);
+});
+
+gulp.task('default', gulp.series('connect', 'watch'));
